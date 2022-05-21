41a03aef74a10316053b0375b3dea35193d98c3d
NeilMankodi <neilmankodi13@gmail.com>
2022-05-21 09:52:09 +0530

    Validations done. Need to prevent form from resetting values on an invalid form input

--- apps/register/models.py
+++ apps/register/models.py
@@ -1,28 +1,14 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class UserManager(models.Manager):
-    def validator(self, postData):
-        errors = {}
-        if (postData['user_name'].isalpha()) == False:
-            if len(postData['user_name']) < 2:
-                errors['user_name'] = "First name can not be shorter than 2 characters"
-
-        if len(postData['email']) == 0:
-            errors['email'] = "You must enter an email"
-
-        if len(postData['password']) < 8:
-            errors['password'] = "Password is too short!"
-
-        return errors
-
 class User(models.Model):
     user_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    mob_number = models.IntegerField(default=0)
+    address = models.CharField(max_length=500, default="")
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-    objects = UserManager()
 
-    def _str_(self):
+    def __str__(self):
         return self.user_name
--- apps/register/views.py
+++ apps/register/views.py
@@ -1,23 +1,92 @@
+from distutils.log import error
+import email
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from hashlib import sha256
 from .models import User
+import re
+
+def mob_number_validator(mob_number):
+    mob_number_messages = []
+    if len(mob_number) != 10:
+        mob_number_messages.append("Invalid Mobile Number! Mobile number has to be 10 digits long.")
+    return mob_number_messages
+
+def email_validator(email):
+    email_messages = []
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if not re.fullmatch(regex, email):
+        email_messages.append("Invalid Email! Email must have an @ symbol followed by the domain.")
+    return email_messages
+
+def password_validator(password):
+    password_messages = []
+    valid = True
+
+    if len(password) < 12:
+        password_messages.append("Password must be greater than 12 characters")
+    
+    if not any(char.isdigit() for char in password):
+        password_messages.append("Password must contain atleast 1 number")
+
+    if not any(char.isupper() for char in password):
+        password_messages.append("Password must contain alteast 1 upper case character")
+
+    if not any(char.islower() for char in password):
+        password_messages.append("Password must contain alteast 1 lower case character")
+
+    if not any(char in ['$', '@', '#', '!', '%'] for char in password):
+        password_messages.append("Password must contain alteast 1 special character")
+
+    return password_messages
+
+def confirm_password_validator(password, confirm_password):
+    confirm_password_messages = []
+    if confirm_password != password:
+        confirm_password_messages.append("Password & Confirm Password must match.")
+    return confirm_password_messages
+
+
 
 def index(request):
     return render(request, 'register/index.html')
 
 def register(request):
-    errors = User.objects.validator(request.POST)
-    if len(errors):
-        for tag, error in errors.iteritems():
-            messages.error(request, error, extra_tags=tag)
-        return redirect('/')
+    mob_number_messages = mob_number_validator(request.POST['mob_number'])
+    email_messages = email_validator(request.POST['email'])
+    password_messages = password_validator(request.POST['password'])
 
-    hashed_password = sha256(request.POST['password'].encode()).hexdigest()
-    user = User.objects.create(user_name=request.POST['user_name'], password=hashed_password, email=request.POST['email'])
-    user.save()
-    request.session['id'] = user.id
-    return redirect('/success')
+    error_messages = {}
+    valid = True
+
+    if len(mob_number_messages) > 0:
+        valid = False
+        error_messages['mob_number_messages'] = mob_number_messages[0]
+
+    if len(email_messages) > 0:
+        valid = False
+        error_messages['email_messages'] = email_messages[0]
+
+    if len(password_messages) > 0:
+        valid = False
+        error_messages['password_messages'] = password_messages
+    
+    if len(password_messages) == 0:
+        confirm_password_messages = confirm_password_validator(request.POST['password'], request.POST['confirm_password'])
+        if len(confirm_password_messages) > 0:
+            valid = False
+            error_messages['confirm_password_messages'] = confirm_password_messages[0]
+
+    if valid:
+        hashed_password = sha256(request.POST['password'].encode()).hexdigest()
+        user = User.objects.create(user_name=request.POST['user_name'], password=hashed_password, email=request.POST['email'], mob_number=request.POST['mob_number'], address=request.POST['address'])
+        user.save()
+        request.session['id'] = user.id
+        return redirect('/success')
+    
+    else:
+        # messages = error_messages
+        return render(request, 'register/index.html', error_messages)
 
 def login(request):
     if (User.objects.filter(user_name=request.POST['user_name']).exists()):
